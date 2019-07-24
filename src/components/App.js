1ce1623077dfe04444f5084847444d71677d90f1
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-07-24 11:06:14 +0200

    rearrange and add empty files

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import './components.css';
+import './style.css';
 
 class App extends React.Component {
   render(){
